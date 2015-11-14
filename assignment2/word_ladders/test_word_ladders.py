d0713429e5a67de90da2d89ff97f1386a8e32ea2
Matthew Ankerson <sm.ankerson@gmail.com>
2015-11-14 14:09:15 +1300

    word ladder using queue and dictionary

--- assignment2/word_ladders/test_word_ladders.py
+++ assignment2/word_ladders/test_word_ladders.py
@@ -7,12 +7,27 @@ class TestWordLadderSolver(unittest.TestCase):
     def setUp(self):
         self.solver = word_ladders.WordLadderSolver()
 
-    def test_solve(self):
-        start = 'fool'
-        end = 'wise'
-        expected = ['fool', 'pool', 'poll', 'pill', 'will', 'wile', 'wise']
-        # actual = self.solver.solve(start, end)
-        # self.assertItemsEqual(expected, actual)
+    def test_solve_1(self):
+        start = 'hello'
+        end = 'pupal'
+        expected = ['pupal', 'pupas', 'pumas', 'pumps', 'pulps', 'pulls',
+                    'hulls', 'hullo', 'hello']
+        actual = self.solver.solve(start, end)
+        self.assertItemsEqual(expected, actual)
+
+    def test_solve_2(self):
+        start = 'baldy'
+        end = 'wonky'
+        expected = ['wonky', 'honky', 'hanky', 'handy', 'bandy', 'baldy']
+        actual = self.solver.solve(start, end)
+        self.assertItemsEqual(expected, actual)
+
+    def test_solve_3(self):
+        start = 'baldy'
+        end = 'ninja'
+        expected = 'No path found.'
+        actual = self.solver.solve(start, end)
+        self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
     unittest.main()
--- assignment2/word_ladders/word_ladders.py
+++ assignment2/word_ladders/word_ladders.py
@@ -1,11 +1,13 @@
+from queue import Queue
 
 
 class WordLadderSolver(object):
 
     def __init__(self):
+        self.unprocessed_q = Queue()
         self.alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        self.ladder_dict = {}
         self.legal_words = set()
-        self.graph = {}
         with open('dictionary.txt', 'r') as f:
             for word in [line.rstrip('\n') for line in f]:
                 self.legal_words.add(word)
@@ -18,11 +20,37 @@ class WordLadderSolver(object):
                     pass
                 else:
                     new_word = word[:i] + x + word[i + 1:]
-                    if new_word in self.legal_words:
+                    if new_word in self.legal_words and\
+                            new_word not in self.ladder_dict:
                         yield new_word
 
     def solve(self, start, end):
-        self.graph = {}
+        # put start word in queue.
+        self.unprocessed_q.enqueue(start)
+        check_end = start
+        self.ladder_dict[start] = None
+        # loop until queue is empty.
+        while not self.unprocessed_q.is_empty() and check_end != end:
+            # pop the next word from the queue.
+            next_word = self.unprocessed_q.dequeue()
+            # add derivs of next_word to queue.
+            for deriv in self.direct_derivs(next_word):
+                self.unprocessed_q.enqueue(deriv)
+                self.ladder_dict[deriv] = next_word
+            check_end = next_word
+        # did we find a path to the end word?
+        if check_end == end:
+            # return the path.
+            path = [end]
+            next_key = self.ladder_dict[end]
+            while next_key is not None:
+                path.append(next_key)
+                next_key = self.ladder_dict[next_key]
+            return path
+        else:
+            return 'No path found.'
 
 if __name__ == '__main__':
     solver = WordLadderSolver()
+    output = solver.solve('baldy', 'wonky')
+    print output
