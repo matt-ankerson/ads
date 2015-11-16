4416572e96bc9ee283bc6b4b608af9d29191c499
Matthew Ankerson <sm.ankerson@gmail.com>
2015-11-16 21:40:27 +1300

    heap sort now working, with help from Isai Damier at geekviewpoint.com

--- assignment2/heapsort/heap_sorter.py
+++ assignment2/heapsort/heap_sorter.py
@@ -23,35 +23,35 @@ class HeapSorter(object):
         '''Swap the elements at index i and j.'''
         lst[i], lst[j] = lst[j], lst[i]
 
-    def _upheap(self, j, lst):
-        parent = self._parent(j)
-        if j > 0 and lst[j] > lst[parent]:
-            self._swap(j, parent, lst)
-            self._upheap(j, lst)
-
-    def _downheap(self, j, lst):
-        if self._has_left(j, lst):
-            left = self._left(j)
-            large_child = left
-            if self._has_right(j, lst):
-                right = self._right(j)
-                if lst[right] > lst[left]:
-                    large_child = right
-            if lst[large_child] > lst[j]:
-                self._swap(j, large_child, lst)
-                self._downheap(j, lst)
+    def _downheap(self, first, last, s):
+        largest = self._left(first)     # assume left child is largest.
+        while largest <= last:
+            # if right child exists and is larger:
+            if largest < last and s[largest] < s[largest + 1]:
+                largest = self._right(first)
+            # if child is larger than parent:
+            if s[largest] > s[first]:
+                self._swap(largest, first, s)
+                # move down
+                first = largest
+                largest = self._left(first)
+            else:
+                return
 
     def sort(self, s):
         '''Heapsort the sequence in place.'''
-        # phase 1: convert the sequence to a heap.
-        # Move the boundary from left to right, one step at a time.
-        for i in range(1, len(s) + 1):
-            self._upheap(i - 1, s)
-
-        # phase 2: move the boundary from right to left, one step at a time.
-        for i in range(len(s), -1, -1):
-            self._swap(0, i - 1, s)
-            self._downheap(0, s)
+        # phase 1: Convert the sequence to a heap.
+        n = len(s) - 1
+        # least_parent = n / 2
+        least_parent = self._parent(len(s))
+        for i in range(least_parent, -1, -1):
+            self._downheap(i, n, s)
+        # phase 2: Convert the heap back to a sequence.
+        # Move the boundary from right to left, one step at a time.
+        for i in range(n, 0, -1):
+            if s[0] > s[i]:
+                self._swap(0, i, s)
+                self._downheap(0, i - 1, s)
 
 if __name__ == '__main__':
     s = [6, 7, 5, 9, 2, 4]
