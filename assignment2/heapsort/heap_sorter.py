ba40578e009fd03c6f675d3485799648733ed25d
Matthew Ankerson <sm.ankerson@gmail.com>
2015-11-16 21:15:28 +1300

    refactored heap sort, still not operational

--- assignment2/heapsort/heap_sorter.py
+++ assignment2/heapsort/heap_sorter.py
@@ -1,15 +1,8 @@
 
 class HeapSorter(object):
 
-    def __init__(self, unsorted_sequence=None):
-        self._data = unsorted_sequence
-        self._boundary = 0
-
-    def __len__(self):
-        return len(self._data[: self._boundary])
-
-    def is_empty(self):
-        return len(self) == 0
+    def __init__(self):
+        pass
 
     def _parent(self, j):
         return (j - 1) / 2
@@ -20,65 +13,49 @@ class HeapSorter(object):
     def _right(self, j):
         return 2 * j + 2
 
-    def _has_left(self, j):
-        return self._left(j) < len(self)
+    def _has_left(self, j, lst):
+        return self._left(j) < len(lst)
 
-    def _has_right(self, j):
-        return self._right(j) < len(self)
+    def _has_right(self, j, lst):
+        return self._right(j) < len(lst)
 
-    def _swap(self, i, j):
+    def _swap(self, i, j, lst):
         '''Swap the elements at index i and j.'''
-        self._data[i], self._data[j] = self._data[j], self._data[i]
+        lst[i], lst[j] = lst[j], lst[i]
 
-    def _upheap(self, j):
+    def _upheap(self, j, lst):
         parent = self._parent(j)
-        if j > 0 and self._data[j] > self._data[parent]:
-            self._swap(j, parent)
-            self._upheap(j)
+        if j > 0 and lst[j] > lst[parent]:
+            self._swap(j, parent, lst)
+            self._upheap(j, lst)
 
-    def _downheap(self, j):
-        if self._has_left(j):
+    def _downheap(self, j, lst):
+        if self._has_left(j, lst):
             left = self._left(j)
             large_child = left
-            if self._has_right(j):
+            if self._has_right(j, lst):
                 right = self._right(j)
-                if self._data[right] > self._data[left]:
+                if lst[right] > lst[left]:
                     large_child = right
-            if self._data[large_child] > self._data[j]:
-                self._swap(j, large_child)
-                self._downheap(j)
-
-    def get_heap(self):
-        items = []
-        for item in self._data[: self._boundary]:
-            items.append(item)
-        return items
-
-    def get_sequence(self):
-        items = []
-        for item in self._data[self._boundary:]:
-            items.append(item)
-        return items
+            if lst[large_child] > lst[j]:
+                self._swap(j, large_child, lst)
+                self._downheap(j, lst)
 
     def sort(self, s):
         '''Heapsort the sequence in place.'''
-        self._data = s
-        # phase 1: move the boundary from left to right, one step at a time.
-        for i in range(1, len(self._data)):
-            self._upheap(i - 1)
-            self._boundary = i
-
-        print 'heap: ' + str(self.get_heap())
+        # phase 1: convert the sequence to a heap.
+        # Move the boundary from left to right, one step at a time.
+        for i in range(1, len(s) + 1):
+            self._upheap(i - 1, s)
 
         # phase 2: move the boundary from right to left, one step at a time.
-        for i in range(len(self._data) - 1, -1, -1):
-            self._boundary = i
-            self._swap(0, len(self) - 1)
-            self._downheap(0)
-
-        print 'sequence: ' + str(self.get_sequence())
+        for i in range(len(s), -1, -1):
+            self._swap(0, i - 1, s)
+            self._downheap(0, s)
 
 if __name__ == '__main__':
     s = [6, 7, 5, 9, 2, 4]
+    print s
     heapsorter = HeapSorter()
     heapsorter.sort(s)
+    print s
